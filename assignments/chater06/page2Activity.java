56c95c590515796a92628f4b0a772e05f76ded87
jinra <140481147+Niceguyhelpsbeginner@users.noreply.github.com>
2025-06-13 10:52:12 +0900

    Create page2Activity.java

---
+++ assignments/chater06/page2Activity.java
@@ -0,0 +1,25 @@
+// Page2Activity.java
+package com.example.navigationchallenge;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Page2Activity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // BACK 키를 눌렀을 때 메인 페이지로 돌아가기
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish(); // 현재 액티비티를 종료하여 메인 페이지로 돌아감
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
